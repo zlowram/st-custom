5fdf79d1b04f06d2eb0bbb22d351abffabadd5b7
zlowram <zlowram@gmail.com>
2020-02-08 22:13:12 +0100

    Patched with alpha + dark pastels theme + scrollback

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono:style=Book:pixelsize=12";
 static int borderpx = 2;
 
 /*
@@ -87,43 +87,49 @@ float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 normal colors */
+  [0] = "#3f3f3f", /* black   */
+  [1] = "#705050", /* red     */
+  [2] = "#60b48a", /* green   */
+  [3] = "#dfaf8f", /* yellow  */
+  [4] = "#9ab8d7", /* blue    */
+  [5] = "#dc8cc3", /* magenta */
+  [6] = "#8cd0d3", /* cyan    */
+  [7] = "#dcdcdc", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#709080", /* black   */
+  [9]  = "#dca3a3", /* red     */
+  [10] = "#72d5a3", /* green   */
+  [11] = "#f0dfaf", /* yellow  */
+  [12] = "#94bff3", /* blue    */
+  [13] = "#ec93d3", /* magenta */
+  [14] = "#93e0e3", /* cyan    */
+  [15] = "#ffffff", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+  /* special colors */
+  [256] = "#030713", /* background */
+  [257] = "#dcdcdc", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
+
 
 /*
  * Default shape of cursor
